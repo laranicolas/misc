bf4efa26160627bbf65386f85d029fb05762f222
JuanJo Ciarlante <juanjosec@gmail.com>
2015-03-18 11:42:48 -0300

    cosmetic

--- 2015/03/18/latency/latency.py
+++ 2015/03/18/latency/latency.py
@@ -7,25 +7,25 @@ def request_ok(current, th_min, th_max):
     """return OK with a probability proportional to
        'low' current, between th_min and th_max
 
-request_ok()
-
-  FALSE       |          /
----------------         *
-  TRUE 25%    -        /
-       ...    |       /
-  TRUE 50% - - - - - +
-       ...    |     /:
-  TRUE 75%    -    / :
---------------|   *  :
-  TRUE        |  /   :
-              | /    :
-              +---|--|--|--------------
-                  |  |  th_max
-                  |  current
-                  th_min
-e.g. for current in the middle between th_min, th_max
-randomly return TRUE half ot the time
-"""
+    request_ok()  |
+                  |
+      FALSE       |          /
+    ---------------         +
+      TRUE 25%    -        /
+           ...    |       /
+      TRUE 50% - - - - - -
+           ...    |     /:
+      TRUE 75%    -    / :
+    --------------|   +  :
+      TRUE        |  /   :
+                  | /    :
+                  +---|--|--|--------------
+                      |  |  th_max
+                      |  current
+                      th_min
+    e.g. for current in the middle between th_min, th_max
+    randomly return TRUE half ot the time
+    """
 
     # change below by fixed implementation, and run
     # make tests
