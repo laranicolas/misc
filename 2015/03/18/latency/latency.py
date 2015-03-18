0ba378477beaaa97610961df69b49504662d1463
JuanJo Ciarlante <juanjosec@gmail.com>
2015-03-18 11:39:46 -0300

    commit latency_test framework for RED

---
+++ 2015/03/18/latency/latency.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"RED (random early drop) simple implementation"
+import random
+
+
+def request_ok(current, th_min, th_max):
+    """return OK with a probability proportional to
+       'low' current, between th_min and th_max
+
+request_ok()
+
+  FALSE       |          /
+---------------         *
+  TRUE 25%    -        /
+       ...    |       /
+  TRUE 50% - - - - - +
+       ...    |     /:
+  TRUE 75%    -    / :
+--------------|   *  :
+  TRUE        |  /   :
+              | /    :
+              +---|--|--|--------------
+                  |  |  th_max
+                  |  current
+                  th_min
+e.g. for current in the middle between th_min, th_max
+randomly return TRUE half ot the time
+"""
+
+    # change below by fixed implementation, and run
+    # make tests
+    return True
